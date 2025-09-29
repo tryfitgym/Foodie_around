0c66026e4085612e31002347318eacffc4ed2674
tryfitgym <152852449+tryfitgym@users.noreply.github.com>
2025-09-29 11:47:51 +1000

    Add files via upload

---
+++ android/build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
